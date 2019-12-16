bcb27982c6bce36283a6d683dee2314e5aa6f759
shawon <speedbreaker350@gmail.com>
2019-12-16 21:51:20 +0600

    Added a new method called display

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("VS code is awesome");
+
+        greeting("Shawon Hossain");
+    }
+
+    public static void greeting(String name){
+        System.out.println("Welcome "+name);
+
     }
 }
